3735048c1703f31ff17f17e81ea991f5fd26e795
Swagat <swagatswaroop@gmail.com>
2024-02-04 02:13:08 +0530

    Refactor App component

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,18 +1,8 @@
-import GeneralInfo from './components/generalinfo'
-import Header from './components/header'
 
-const AssesmentForm = () => {
-  return (
-    <>
-      <div className="flex flex-col items-center ml-8 mr-8 border-black border-2 mt-8">
-        <div className="mt-20 mb-8 border-b-2 border-black">
-          <Header />
-        </div>
-        <header className="text-2xl text-center mb-3">ASSESSMENT FORM</header>
-        <GeneralInfo />
-      </div>
-    </>
-  )
+
+const App = () => {
+  
 }
 
-export default AssesmentForm
+
+export default App
